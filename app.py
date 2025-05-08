a3eedc1625aa77207941649d43d499614f4a44f4
jaspreetkaur230790 <2024tm93052@wilp.bits-pilani.ac.in>
2025-05-08 20:30:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ def payment_gateway():
     return render_template('payment_gateway.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
